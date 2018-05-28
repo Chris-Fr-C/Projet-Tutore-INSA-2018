8baf9b12464f9ef318acca73852ea82d872d8efb
Corsi Christophe <corsi@etud.insa-toulouse.fr>
2018-05-28 12:17:27 +0200

    Ajout de l'optimisation

---
+++ modeles/modeles.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Module qui vise a construire les modèles simplifiés pour l'optimisation dans le
+cadre du projet tutoré INSA 2018 encadré par Marc Budinger.
+Le code ne vise pas à être optimisé, mais simplement compréhensible par des
+ingénieurs n'ayant que très peu de compréhension du language de programmation,
+ni des protocoles de machine learning, comme demandé par l'encadrant.
+
+@author: Christophe Corsi
+"""
+
+models = dict()
+
+
